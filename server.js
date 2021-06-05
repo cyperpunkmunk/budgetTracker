74fee272708e2feeeccf3e50f3f445e2c0c9910c
Kenneth Gibson jr <Kendgibsonjr@gmail.com>
2021-06-04 23:56:06 -0500

    deploying

--- server.js
+++ server.js
@@ -16,13 +16,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  
-});
+  process.env.MONGODB_URI || 'mongodb://localhost/budget', 
+  {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
